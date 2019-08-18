0796423da0b89211dc07e0f0cf5aac151e8aa67f
Dat Chung <datchung@users.noreply.github.com>
2019-08-17 23:12:43 -0400

    Add framework ParseCsv test

--- src/TextFieldParserFramework.Test/TextFieldParserFrameworkTest.cs
+++ src/TextFieldParserFramework.Test/TextFieldParserFrameworkTest.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TextFieldParserFramework.Test
 {
+    [TestFixture]
     public class TextFieldParserFrameworkTest
     {
+        [Test]
+        public void ParseCsv()
+        {
+            var parsedRows = new List<string[]>();
+            using (var streamReader = new StreamReader(StringToStream(
+@"Col1,Col2,Col3
+A,B,C
+D,E,F")))
+            {
+                using (var parser = new TextFieldParser(streamReader))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+
+                    while (!parser.EndOfData)
+                    {
+                        parsedRows.Add(parser.ReadFields());
+                    }
+                }
+            }
+
+            Assert.AreEqual(3, parsedRows.Count);
+            var i = 0;
+            var j = 0;
+            var row = parsedRows[i];
+            Assert.AreEqual(3, row.Length, $"parsedRows[{i}] length");
+            Assert.AreEqual("Col1", row[j], $"parsedRows[{i}] col[{j}]");
+            Assert.AreEqual("Col2", row[++j], $"parsedRows[{i}] col[{j}]");
+            Assert.AreEqual("Col3", row[++j], $"parsedRows[{i}] col[{j}]");
+
+            j = 0;
+            row = parsedRows[++i];
+            Assert.AreEqual(3, row.Length, $"parsedRows[{i}] length");
+            Assert.AreEqual("A", row[j], $"parsedRows[{i}] col[{j}]");
+            Assert.AreEqual("B", row[++j], $"parsedRows[{i}] col[{j}]");
+            Assert.AreEqual("C", row[++j], $"parsedRows[{i}] col[{j}]");
+
+            j = 0;
+            row = parsedRows[++i];
+            Assert.AreEqual(3, row.Length, $"parsedRows[{i}] length");
+            Assert.AreEqual("D", row[j], $"parsedRows[{i}] col[{j}]");
+            Assert.AreEqual("E", row[++j], $"parsedRows[{i}] col[{j}]");
+            Assert.AreEqual("F", row[++j], $"parsedRows[{i}] col[{j}]");
+        }
+
+        /// <summary>
+        /// http://www.csharp411.com/c-convert-string-to-stream-and-stream-to-string/
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private static Stream StringToStream(string str)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(str));
+        }
     }
 }
